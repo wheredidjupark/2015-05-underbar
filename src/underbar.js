5685a9b9162bcd91e4411798539e833b7d8bf079
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-03-14 10:57:04 -0700

    .

--- src/underbar.js
+++ src/underbar.js
@@ -220,21 +220,23 @@ return result;
   _.contains = function(collection, target) {
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
-    /*
+    
     return _.reduce(collection, function(wasFound, item) {
       if (wasFound) {
         return true;
       }
       return item === target;
     }, false);
-*/
 
+
+/*
     for(var key in collection){
       if(collection[key] === target){
         return true;
       }
     }
     return false;
+    */
   };
 
 
